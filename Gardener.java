7417864545c60e7dac63721873378caa74ee3a1e
Ben Black <weepingwillowben@gmail.com>
2017-01-11 19:49:05 -0700

    lumberjack and troop improvements

--- Gardener.java
+++ Gardener.java
@@ -1,13 +1,15 @@
 package benplayer;
 import battlecode.common.*;
+import battlecode.world.TeamInfo;
 
 public class Gardener extends BaseRobot{
-    public Gardener(RobotController rc) {
-        super(rc);
+    public Gardener(RobotController inrc) {
+        super(inrc);
     }
 
     boolean tree_built = false;
-    int wander_timer = 20;
+    int wander_timer = 1;
+    boolean built_lumberjack = false;
     @Override
     public void run() throws GameActionException {
         super.run();
@@ -17,6 +19,8 @@ public class Gardener extends BaseRobot{
         int yPos = rc.readBroadcast(1);
         MapLocation archonLoc = new MapLocation(xPos, yPos);
 
+        set_wander_movement();
+        //builds tree if not wandering
         if (wander_timer > 0) {
             if (buildTreeRand()) {
                 tree_built = true;
@@ -26,10 +30,33 @@ public class Gardener extends BaseRobot{
         }
         water_tree();
         produce_soldiers();
+        produce_lumberjack();
 
-        // Move randomly
+        //if not sticking to tree, then move
         if(!tree_built){
-            tryMove(randomDirection());
+            //try to move optimally
+            if(!moveOpti()) {
+                // try to move randomly
+                tryMove(randomDirection());
+            }
+        }
+    }
+    void avoid_directions_blocked()throws GameActionException{
+        final int dir_cnt = 20;
+        final MapLocation cen = rc.getLocation();
+        Direction dir = new Direction(0);
+        for(int i = 0; i < dir_cnt; i++){
+            dir = dir.rotateLeftRads((float)(Math.PI * 2 / dir_cnt));
+            MapLocation loc = cen.add(dir);
+            if(!rc.isLocationOccupiedByTree(loc)){
+                movement.add_liniar_pull(loc,Const.GAR_WAND_BLOCKED_VAL);
+            }
+        }
+    }
+    void set_wander_movement() throws GameActionException{
+        avoid_directions_blocked();
+        for(MapLocation aloc : nearbyArchons()){
+            movement.add_liniar_pull(aloc,Const.GAR_WAND_ARCHON_AVOID);
         }
     }
     boolean water_tree() throws GameActionException{
@@ -49,6 +76,21 @@ public class Gardener extends BaseRobot{
         if(rc.senseNearbyRobots(-1,rc.getTeam().opponent()).length > 0 ||
                 (rc.getTeamBullets() > 1000 && Math.random() < 0.01)){
             tryBuildRand(RobotType.SOLDIER);
+        }
+    }
+    float neutral_tree_area(){
+        float areasum = 0;
+        for(TreeInfo tree : rc.senseNearbyTrees(-1,Team.NEUTRAL)){
+            areasum += Const.area(tree.radius);
+        }
+        return areasum;
+    }
+    void produce_lumberjack() throws GameActionException {
+        float neut_tree_percent = neutral_tree_area() / Const.area(rc.getType().sensorRadius);
+        if(!built_lumberjack && neut_tree_percent > Const.LUMBER_TREE_AREA_HIRE_PERC){
+            if(tryBuildRand(RobotType.LUMBERJACK)){
+                built_lumberjack = true;
+            }
         }
     }
 }
---
+++ Lumberjack.java
@@ -0,0 +1,53 @@
+package benplayer;
+
+import battlecode.common.*;
+
+/**
+ * Created by benblack on 1/11/2017.
+ */
+public class Lumberjack extends BaseRobot {
+    public Lumberjack(RobotController inrc){
+        super(inrc);
+    }
+
+    @Override
+    public void run() throws GameActionException {
+        super.run();
+
+        Team enemy = rc.getTeam().opponent();
+        // See if there are any enemy robots within striking range (distance 1 from lumberjack's radius)
+        RobotInfo[] robots = rc.senseNearbyRobots(RobotType.LUMBERJACK.bodyRadius+GameConstants.LUMBERJACK_STRIKE_RADIUS, enemy);
+
+        if(false && robots.length > 0 && !rc.hasAttacked()) {
+            // Use strike() to hit all nearby robots!
+            rc.strike();
+        } else {
+            // No close robots, so search for robots within sight radius
+            robots = rc.senseNearbyRobots(-1,enemy);
+
+            // If there is a robot, move towards it
+            if(false && robots.length > 0) {
+                MapLocation myLocation = rc.getLocation();
+                MapLocation enemyLocation = robots[0].getLocation();
+                Direction toEnemy = myLocation.directionTo(enemyLocation);
+
+                tryMove(toEnemy);
+            } else {
+                move_to_tree();
+                if(!moveOpti()) {
+                    // Move Randomly
+                    tryMove(randomDirection());
+                }
+            }
+        }
+    }
+    public void move_to_tree() throws GameActionException {
+        for(TreeInfo tree : rc.senseNearbyTrees(-1,Team.NEUTRAL)){
+            if(rc.canChop(tree.ID)){
+                rc.chop(tree.ID);
+            }
+            float dis_tree = rc.getLocation().distanceTo(tree.location);
+            movement.add_liniar_pull(tree.location,Const.LUMBER_TREE_LOC_BASE/dis_tree);
+        }
+    }
+}
